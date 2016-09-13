bca6016529b8568fd26a785b9867d020ea4a73f1
Ahri Fox <darkomega950@gmail.com>
2016-09-13 11:11:47 -0400

    Refined code.

--- AhriCloud Client/.src/Form1.Designer.cs
+++ AhriCloud Client/.src/Form1.Designer.cs
@@ -28,20 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LogOut = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(106, 45);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ahricloud.ddns.net";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LogOut);
+            this.groupBox2.Location = new System.Drawing.Point(126, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(769, 369);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.Input);
+            this.groupBox3.Location = new System.Drawing.Point(126, 388);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(769, 41);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Input";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(7, 15);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(667, 20);
+            this.Input.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(681, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LogOut
+            // 
+            this.LogOut.FormattingEnabled = true;
+            this.LogOut.Location = new System.Drawing.Point(6, 19);
+            this.LogOut.Name = "LogOut";
+            this.LogOut.Size = new System.Drawing.Size(750, 342);
+            this.LogOut.TabIndex = 0;
+            this.LogOut.SelectedIndexChanged += new System.EventHandler(this.LogOut_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(856, 420);
+            this.ClientSize = new System.Drawing.Size(907, 441);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.ListBox LogOut;
     }
 }
 
--- AhriCloud Client/.src/Form1.cs
+++ AhriCloud Client/.src/Form1.cs
@@ -12,9 +12,77 @@ namespace AhriCloud_Client
 {
     public partial class Form1 : Form
     {
+        private static Action NonStaticDelegate;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// This is the method that will return the log form.
+        /// </summary>
+        /// <param name="message">The message to be sent.</param>
+        /// <param name="action">The action stamp.</param>
+        private void log(string message, string action)
+        {
+            // First, cache all of the data.
+            string _timestamp = DateTime.Now.ToString();
+            int _actionnumber = _Data.action;
+            string to_return = String.Format("[{0}} [{1}] <{2}> %{3}%",
+                action, _actionnumber, _timestamp, message);
+
+            // Then update the action number.
+            _Data.action += 1;
+
+            // Finally return the final string.
+            LogOut.BeginInvoke(new Action (delegate{
+                LogOut.Items.Add(to_return);
+            }));
+
+
+        }
+        protected class _Data
+        {
+            public static int action = 0;
+        }
+
+        /// <summary>
+        /// This holds all of the logic.
+        /// </summary>
+        public static class Logic
+        {
+            /// <summary>
+            /// This interperts all of the data.
+            /// </summary>
+            /// <param name="data">The incoming data.</param>
+            public void RecData(string data)
+            {
+                log
+            }
+        }
+
+        /// <summary>
+        /// This is the response to the button being pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // When the button is clicked, send the data to the system.
+            // First, cache in the information, and then clear the text.
+            string input = Input.Text;
+            Input.Text = "";
+
+            // Send the data.
+            Logic.RecData(input);
+            
+        }
+
+        private void LogOut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+
 }
